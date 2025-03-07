01a231234e19501c4d1cf23ed447e4824e4051c3
Shekh Mahdi Mesbah <mahdisheikh1624@gmail.com>
2025-03-08 00:14:11 +0600

    completed the methods

--- Expence Tracker/src/main/java/ExpenseIncomeTableModel.java
+++ Expence Tracker/src/main/java/ExpenseIncomeTableModel.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.List;
+
 import javax.swing.table.AbstractTableModel;
 
 public class ExpenseIncomeTableModel extends AbstractTableModel{
@@ -11,5 +13,52 @@ public class ExpenseIncomeTableModel extends AbstractTableModel{
     public void addEntry(ExpenseIncomeEntry entry){
         entries.add(entry);
         fireTableRowsInserted(entries.size() - 1, entries.size() - 1);
+    }
+
+    public void editEntry(int rowIndex, ExpenseIncomeEntry updatedEntry) {
+        entries.set(rowIndex, updatedEntry);
+        fireTableRowsUpdated(rowIndex, rowIndex);
+    }
+
+    public void removeEntry(int rowIndex) {
+        entries.remove(rowIndex);
+        fireTableRowsDeleted(rowIndex, rowIndex);
+    }
+
+    private Object getColumnValue(ExpenseIncomeEntry entry, int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return entry.getDate();
+            case 1:
+                return entry.getCategory();
+            case 2:
+                return entry.getAmount();
+            case 3:
+                return entry.getType();
+            default:
+                return null;
+        }
+    }
+
+
+    @Override
+    public int getRowCount() {
+        return entries.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column];
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        ExpenseIncomeEntry entry = entries.get(rowIndex);
+        return getColumnValue(entry, columnIndex);
     }
 }
